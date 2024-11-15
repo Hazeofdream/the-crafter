8d65ced6dbe18a66b93c56b77791c00df1472f7f
Haze_of_dream <74027566+Hazeofdream@users.noreply.github.com>
2024-11-15 01:24:49 -0600

    Properly fix Hawk Times

--- The crafter 3.2.1/user/mods/The crafter/src/mod.ts
+++ The crafter 3.2.1/user/mods/The crafter/src/mod.ts
@@ -72,8 +72,7 @@ class the_crafter implements IPostDBLoadMod {
 	// Rebalance
 	if (config.CraftingRebalance == true) {
 		// Lower Gunpowder Times
-		getCraft("5d6fc87386f77449db3db94e").endProduct.forEach((x) => x.endProduct == "5d6fc87386f77449db3db94e").productionTime = 900  // Hawk
-		//tables.hideout.production.forEach((x) => x.endProduct == "5d6fc87386f77449db3db94e" && x.areaType != 21).productionTime = 900
+		tables.hideout.production.filter((x) => x.endProduct == "5d6fc87386f77449db3db94e" && x.areaType != 21).productionTime = 900
 		getCraft("5d6fc78386f77449d825f9dc").productionTime = 900 // Eagle
 		getCraft("590c5a7286f7747884343aea").productionTime = 900 // Kite
 
