857d3abd86e34029327c5069c15d11dabf3a0c01
Haze_of_dream <74027566+Hazeofdream@users.noreply.github.com>
2024-11-16 20:16:35 -0600

    push

--- The crafter 3.2.1/user/mods/The crafter/src/mod.ts
+++ The crafter 3.2.1/user/mods/The crafter/src/mod.ts
@@ -72,7 +72,7 @@ class the_crafter implements IPostDBLoadMod {
 	// Rebalance
 	if (config.CraftingRebalance == true) {
 		// Lower Gunpowder Times
-		tables.hideout.production.filter((x) => x.endProduct == "5d6fc87386f77449db3db94e" && x.areaType != 21).productionTime = 900
+		getCraft("5d6fc87386f77449db3db94e").endProduct.foreach((x) => {if (x.productionTime) {x.productionTime = 3}})
 		getCraft("5d6fc78386f77449d825f9dc").productionTime = 900 // Eagle
 		getCraft("590c5a7286f7747884343aea").productionTime = 900 // Kite
 
